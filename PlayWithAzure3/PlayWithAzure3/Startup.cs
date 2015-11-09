3714b999aac5aff7a40ea2b7c0dfe161f27e5326
MauricioD <mauricio.dominguez@bizagi.com>
2015-11-09 11:22:27 -0500

    Project creation

---
+++ PlayWithAzure3/PlayWithAzure3/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+using Owin;
+using PlayWithAzure3;
+
+namespace AngularJSWebApiEmpty
+{
+    public class Startup
+    {
+        public void Configure(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            WebApiConfig.Register(config);
+
+            app.UseWebApi(config);
+        }
+    }
+}
